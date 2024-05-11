af47185a2785437374b0b9f086ff4c7c6f38109f
randyisalive <99945506+randyisalive@users.noreply.github.com>
2024-05-12 06:01:37 +0700

    delete

--- flask/oauth.py
+++
@@ -1,6 +0,0 @@
-from ytmusicapi import YTMusic
-import pprint
-
-yt = YTMusic("oauth.json")  # Replace with your authentication details
-search_results = yt.search("Oasis Wonderwall")
-pprint.pprint(search_results)
